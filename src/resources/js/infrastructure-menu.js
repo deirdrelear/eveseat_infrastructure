1cc81e3e8fb8577056bb0331d05e66bd54dd38f3
deirdrelear <rb.18@yandex.ru>
2024-10-20 15:43:19 +0300

    fixes

--- src/resources/js/infrastructure-menu.js
+++ src/resources/js/infrastructure-menu.js
@@ -5,26 +5,24 @@ $(document).ready(function() {
     var menuState = localStorage.getItem('menuState');
     console.log('Initial menu state:', menuState);
 
+    if (menuState === null) {
+        // Если состояние не сохранено, используем текущее состояние body
+        menuState = $('body').hasClass('sidebar-collapse') ? 'collapsed' : 'expanded';
+        localStorage.setItem('menuState', menuState);
+    }
+
+    // Устанавливаем начальное состояние меню
     if (menuState === 'expanded') {
-        $('body').removeClass('sidebar-collapse').addClass('sidebar-open');
-    } else if (menuState === 'collapsed') {
-        $('body').addClass('sidebar-collapse').removeClass('sidebar-open');
+        $('body').removeClass('sidebar-collapse');
+    } else {
+        $('body').addClass('sidebar-collapse');
     }
 
     // Обработчик клика по кнопке сворачивания/разворачивания меню
     $('[data-widget="pushmenu"]').on('click', function() {
         console.log('Menu clicked');
-        if ($('body').hasClass('sidebar-collapse')) {
-            localStorage.setItem('menuState', 'expanded');
-            console.log('Menu expanded');
-        } else {
-            localStorage.setItem('menuState', 'collapsed');
-            console.log('Menu collapsed');
-        }
+        menuState = $('body').hasClass('sidebar-collapse') ? 'expanded' : 'collapsed';
+        localStorage.setItem('menuState', menuState);
+        console.log('New menu state:', menuState);
     });
-
-    // Проверяем, существует ли элемент с атрибутом data-widget="pushmenu"
-    if ($('[data-widget="pushmenu"]').length === 0) {
-        console.error('Element with data-widget="pushmenu" not found');
-    }
 });
