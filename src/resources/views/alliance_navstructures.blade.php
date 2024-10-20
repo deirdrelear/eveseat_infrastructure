6a547cbba295fa2a7f94bf4c41971d1c8820c42b
deirdrelear <rb.18@yandex.ru>
2024-10-20 18:49:36 +0300

    fixes

--- src/resources/views/alliance_navstructures.blade.php
+++ src/resources/views/alliance_navstructures.blade.php
@@ -1,34 +1,38 @@
 @extends('infrastructure::layouts.app')
 
-@section('infrastructure_title', 'Alliance Infrastructure: Navigation Structures')
-@section('infrastructure_page_header', 'Alliance Infrastructure: Navigation Structures')
+@section('infrastructure_title', 'Alliance Infrastructure: Metenoxes')
+@section('infrastructure_page_header', 'Alliance Infrastructure: Metenoxes')
 
 @section('infrastructure_content')
-    <table class="table table-striped table-hover" id="allianceNavigationStructuresTable">
+    <table class="table table-striped table-hover" id="allianceminingStructuresTable">
         <thead>
         <tr>
             <th scope="col">Structure Type</th>
             <th scope="col">Name</th>
             <th scope="col">Solar System</th>
+            <th scope="col">Moon</th>
             <th scope="col">Corporation</th>
             <th scope="col">Fuel</th>
+            <th scope="col">Profit</th>
         </tr>
         </thead>
         <tbody>
-        @foreach($navigationStructures as $navigationStructure)
+        @foreach($miningStructures as $miningStructure)
             <tr>
-                <td>{{ $navigationStructure->structure_type->typeName }}</td>
-                <td>{{ $navigationStructure->name }}</td>
-                <td>{{ $navigationStructure->solarSystem->name }}</td>
-                <td>{{ $navigationStructure->corporation->name }}</td>
+                <td>{{ $miningStructure->structure_type->typeName }}</td>
+                <td>{{ $miningStructure->name }}</td>
+                <td>{{ $miningStructure->solarSystem->name }}</td>
+                <td></td>
+                <td>{{ $miningStructure->corporation->name }}</td>
                 <td>
-                    @foreach($navigationStructure->fuels as $fuel)
+                    @foreach($miningStructure->fuels as $fuel)
                         {{ $fuel->fuel_type->typeName }} - {{ $fuel->quantity }}<br>
                     @endforeach
                 </td>
+                <td></td>
             </tr>
         @endforeach
         </tbody>
     </table>
+    <div class="container">
 @endsection
-
