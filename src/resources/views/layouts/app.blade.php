ceaaf967a8eeaf1d7624aff6d7adf329dc854660
deirdrelear <rb.18@yandex.ru>
2025-10-20 21:30:11 +0300

    Add region display to infrastructure tables and align DataTables assets

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 @extends('web::layouts.grids.12')
 
 @push('head')
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.2/css/jquery.dataTables.css">
 @endpush
 
 @section('title')
