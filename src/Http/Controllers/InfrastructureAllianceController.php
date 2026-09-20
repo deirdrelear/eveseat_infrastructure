db8a81559367a62e7eb39fed4f84e09d6bde96d0
deirdrelear <rb.18@yandex.ru>
2026-09-20 17:01:16 +0300

    fix: restore controller syntax and Metenox date handling

--- src/Http/Controllers/InfrastructureAllianceController.php
+++ src/Http/Controllers/InfrastructureAllianceController.php
@@ -1,56 +1,43 @@
 <?php
 
-
 namespace Deirdrelear\Seat\Infrastructure\Http\Controllers;
 
-use Deirdrelear\Seat\Infrastructure\Service;
-use Seat\Web\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Request as RequestFacade;
 use Carbon\Carbon;
+use Deirdrelear\Seat\Infrastructure\Service;
+use Illuminate\Http\Request;
+use Seat\Web\Http\Controllers\Controller;
 
 class InfrastructureAllianceController extends Controller
 {
-    public function ihubs() {
-        // Получаем идентификаторы корпораций, в которых состоят альты пользователя
+    public function ihubs()
+    {
         $userCorporationsIds = Service::getUserCorporationsIds();
-
-        // Получаем идентификаторы альянсов, в которых состоят альты пользователя
         $allianceCorporationsIds = Service::getAllianceCorporationsIds($userCorporationsIds);
-
-        // получаем список ihub'ов нужных альянсов
         $ihubs = Service::getIHubsInSpace($allianceCorporationsIds);
 
-        // выводим шаблон
-        return view("infrastructure::alliance_ihubs", ['ihubs' => $ihubs]);
+        return view('infrastructure::alliance_ihubs', ['ihubs' => $ihubs]);
     }
 
-    public function navstructures() {
-        // Получаем идентификаторы корпораций, в которых состоят альты пользователя
+    public function navstructures()
+    {
         $userCorporationsIds = Service::getUserCorporationsIds();
-
-        // Получаем идентификаторы альянсов, в которых состоят альты пользователя
         $allianceCorporationsIds = Service::getAllianceCorporationsIds($userCorporationsIds);
-
-        // Получаем список навигационных структур нужных альянсов
         $navigationStructures = Service::getNavigationStructuresInSpace($allianceCorporationsIds);
 
-        // выводим шаблон
-        return view("infrastructure::alliance_navstructures", ['navigationStructures' => $navigationStructures]);
+        return view('infrastructure::alliance_navstructures', [
+            'navigationStructures' => $navigationStructures,
+        ]);
     }
 
-    public function dockstructures() {
-        // Получаем идентификаторы корпораций, в которых состоят альты пользователя
+    public function dockstructures()
+    {
         $userCorporationsIds = Service::getUserCorporationsIds();
-
-        // Получаем идентификаторы альянсов, в которых состоят альты пользователя
         $allianceCorporationsIds = Service::getAllianceCorporationsIds($userCorporationsIds);
-
-        // Получаем список структур с доком для заданных альянсов
         $dockingStructures = Service::getDockingStructuresInSpace($allianceCorporationsIds);
 
-        // выводим шаблон
-        return view("infrastructure::alliance_dockstructures", ['dockingStructures' => $dockingStructures]);
+        return view('infrastructure::alliance_dockstructures', [
+            'dockingStructures' => $dockingStructures,
+        ]);
     }
 
     public function miningstructures(Request $request)
@@ -67,10 +54,9 @@ class InfrastructureAllianceController extends Controller
         $allianceCorporationsIds = Service::getAllianceCorporationsIds($userCorporationsIds);
         $miningStructures = Service::getMetenoxStructuresInSpace($allianceCorporationsIds, $targetDate);
 
-        return view("infrastructure::alliance_miningstructures", [
+        return view('infrastructure::alliance_miningstructures', [
             'miningStructures' => $miningStructures,
             'targetDate' => $targetDate,
         ]);
-
-
+    }
 }
